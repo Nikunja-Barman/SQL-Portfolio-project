70fc0e57bde8d61942f4bf7b0b2611999b7127ce
Nikunja-Barman <129973208+Nikunja-Barman@users.noreply.github.com>
2023-05-07 21:34:03 +0530

    Update SQL Portfolio project.sql

--- SQL Portfolio project.sql
+++ SQL Portfolio project.sql
@@ -162,3 +162,69 @@ where d.continent is not null
 
 -- looking the viewed table
 select * from PercentPopulationVaccinated
+
+
+
+
+
+/*
+
+SQL queries for Tableau visualization
+
+*/
+
+-- 1. 
+
+Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as decimal))/SUM(New_Cases)*100 as DeathPercentage
+From Covid_deaths
+--Where location like '%states%'
+where continent is not null 
+--Group By date
+order by 1,2
+
+
+-- Just a double check based off the data provided
+-- numbers are extremely close so we will keep them - The Second includes "International"  Location
+
+
+--Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+--From PortfolioProject..CovidDeaths
+----Where location like '%states%'
+--where location = 'World'
+----Group By date
+--order by 1,2
+
+
+-- 2. 
+
+-- We take these out as they are not inluded in the above queries and want to stay consistent
+-- European Union is part of Europe
+
+Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
+From Covid_Deaths
+--Where location like '%states%'
+Where continent is null 
+and location not in ('World', 'European Union', 'International','High income',
+'Upper middle income','Lower middle income','Low income')
+Group by location
+order by TotalDeathCount desc
+
+-- 3.
+
+Select Location, Population, MAX(total_cases) as HighestInfectionCount,  
+Max((total_cases/cast(population as decimal)))*100 as PercentPopulationInfected
+From Covid_Deaths
+--Where location like '%states%'
+Group by Location, Population
+order by PercentPopulationInfected desc
+
+
+-- 4.
+
+
+Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  
+Max((total_cases/cast(population as decimal)))*100 as PercentPopulationInfected
+From Covid_Deaths
+--Where location like '%states%'
+Group by Location, Population, date
+order by PercentPopulationInfected desc
